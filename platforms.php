14c8e7a32b681c6d6bd563ef8f39951778adf53d
Twinaphex <libretro@gmail.com>
2019-04-13 17:58:02 +0200

    Update platforms.php

--- platforms.php
+++ platforms.php
@@ -27,7 +27,7 @@
                     <a href="https://buildbot.libretro.com/stable/<?php echo $version; ?>/windows/x86/RetroArch-<?php echo $version; ?>-x86-setup.exe" title="If you are unsure which version to install for your Windows computer, chances are you will want to pick this one. Use this if you are running a 32bit version of Windows Vista, Windows 7, Windows 8, Windows 8.1, or Windows 10 on your computer. This is a graphical installer. If you already have RetroArch installed, this installer will simply upgrade your existing installation.">Installer (32bit)</a>
                     <a href="https://buildbot.libretro.com/stable/<?php echo $version; ?>/windows/x86_64/RetroArch.7z" title="The same as the installer version, except packaged inside a 7zip file, in case you'd like to manually update or install RetroArch somewhere. Use this if you are running a 64bit version of Windows Vista, Windows 7, Windows 8, Windows 8.1, or Windows 10 on your computer.">Download (64bit)</a>
                     <a href="https://buildbot.libretro.com/stable/<?php echo $version; ?>/windows/x86/RetroArch.7z" title="The same as the installer version, except packaged inside a 7zip file, in case you'd like to manually update or install RetroArch somewhere. Use this if you are running a 32bit version of Windows Vista, Windows 7, Windows 8, Windows 8.1, or Windows 10 on your computer.">Download (32bit)</a>
-                    <a href="https://docs.libretro.com/compilation/windows/">Compilation instructions (MinGW-W64 MSYS2)</a>
+                    <a href="https://docs.libretro.com/development/retroarch/compilation/windows/">Compilation instructions (MinGW-W64 MSYS2)</a>
                 </div>
             </div>
             <div class="col-md-3 col-sm-6">
@@ -38,7 +38,7 @@
                     <a href="https://buildbot.libretro.com/stable/<?php echo $version; ?>/windows-msvc2010/x86_64/RetroArch-<?php echo $version; ?>-msvc2010-x64-setup.exe" title="Use this is if you are running a computer with Windows XP x64 Edition. This is a graphical installer. If you already have RetroArch installed, this installer will simply upgrade your existing installation. NOTE: While this version will also work for Vista and up, we highly recommend you use the other Windows version instead, as your core choice will be more limited with this version." onclick="return showXPConfirmation()">Installer (64bit)</a>
                     <a href="https://buildbot.libretro.com/stable/<?php echo $version; ?>/windows-msvc2010/x86/RetroArch.7z" title="The same as the installer version, except packaged inside a 7zip file, in case you'd like to manually update or install RetroArch somewhere. Use this if you are running a computer running Windows XP. NOTE: While this version will also work for Vista and up, we highly recommend you use the other Windows version instead, as your core choice will be more limited with this version." onclick="return showXPConfirmation()">Download (32bit)</a>
                     <a href="https://buildbot.libretro.com/stable/<?php echo $version; ?>/windows-msvc2010/x86_64/RetroArch.7z" title="The same as the installer version, except packaged inside a 7zip file, in case you'd like to manually update or install RetroArch somewhere. Use this if you are running a computer running Windows XP. NOTE: While this version will also work for Vista and up, we highly recommend you use the other Windows version instead, as your core choice will be more limited with this version." onclick="return showXPConfirmation()">Download (64bit)</a>
-                    <a href="https://docs.libretro.com/compilation/windowsXP/">Compilation instructions (Visual Studio 2010)</a>
+                    <a href="https://docs.libretro.com/development/retroarch/compilation/windowsXP/">Compilation instructions (Visual Studio 2010)</a>
                 </div>
             </div>
             <div class="col-md-3 col-sm-6">
@@ -47,7 +47,7 @@
                     <h4>Windows 2000 / ME / 98SE</h4>
                     <a href="https://buildbot.libretro.com/stable/<?php echo $version; ?>/windows-msvc2005/x86/RetroArch-<?php echo $version; ?>-msvc2005-x86-setup.exe" title="Use this is if you are running a computer with Windows 98 Second Edition, Windows Millenium Edition, and/or Windows 2000. This is a graphical installer. If you already have RetroArch installed, this installer will simply upgrade your existing installation. NOTE: While this version will also work for XP and up, we highly recommend you use the other Windows version instead, as your core choice will be more limited with this version.">Installer</a>
                     <a href="https://buildbot.libretro.com/stable/<?php echo $version; ?>/windows-msvc2005/x86/RetroArch.7z" title="The same as the installer version, except packaged inside a 7zip file, in case you'd like to manually update or install RetroArch somewhere. Use this if you are running a computer running Windows 98 Second Edition, Windows Millenium Edition, and/or Windows 2000. NOTE: While this version will also work for XP and up, we highly recommend you use the other Windows version instead, as your core choice will be more limited with this version.">Download</a>
-                    <a href="https://buildbot.libretro.com/docs/compilation/windows98/">Compilation instructions (Visual Studio 2005)</a>
+                    <a href="https://docs.libretro.com/development/retroarch/compilation/windows98/">Compilation instructions (Visual Studio 2005)</a>
                 </div>
             </div>
             <div class="col-md-3 col-sm-6">
@@ -56,7 +56,7 @@
                     <h4>Windows 98 / 95</h4>
                     <a href="https://buildbot.libretro.com/stable/<?php echo $version; ?>/windows-msvc2003/x86/RetroArch-<?php echo $version; ?>-msvc2003-x86-setup.exe" title="Use this is if you are running a computer with Windows 95 or Windows 98 First Edition. This is a graphical installer. If you already have RetroArch installed, this installer will simply upgrade your existing installation. NOTE: While this version will also work for 98SE and up, we highly recommend you use the other Windows version instead, as your core choice will be more limited with this version.">Installer</a>
                     <a href="https://buildbot.libretro.com/stable/<?php echo $version; ?>/windows-msvc2003/x86/RetroArch.7z" title="The same as the installer version, except packaged inside a 7zip file, in case you'd like to manually update or install RetroArch somewhere. Use this if you are running a computer running Windows 95 or Windows 98 First Edition. NOTE: While this version will also work for 98SE and up, we highly recommend you use the other Windows version instead, as your core choice will be more limited with this version.">Download</a>
-                    <a href="https://docs.libretro.com/compilation/windows95-msvc-cmdline/">Compilation instructions (Visual Studio 2003)</a>
+                    <a href="https://docs.libretro.com/development/retroarch/compilation/windows95-msvc-cmdline/">Compilation instructions (Visual Studio 2003)</a>
                 </div>
             </div>
         </div>
@@ -66,7 +66,7 @@
                    <img src="images/linux.png">
                    <h4>Linux</h4>
                    <a href="index.php?page=linux-instructions">Installation instructions</a>
-                   <a href="https://docs.libretro.com/compilation/ubuntu/">Compilation instructions (Ubuntu)</a>
+                   <a href="https://docs.libretro.com/development/retroarch/compilation/ubuntu/">Compilation instructions (Ubuntu)</a>
                </div>
             </div>
             <div class="col-md-3 col-sm-6">
@@ -120,7 +120,7 @@
                     <a href="https://buildbot.libretro.com/stable/<?php echo $version; ?>/apple/ios/RetroArch.zip">Download</a>
                     <a href="https://buildbot.libretro.com/stable/<?php echo $version; ?>/apple/ios9/RetroArch.zip">Download (iOS9)</a>
                     <a href="#">Installation instructions</a>
-                    <a href="https://docs.libretro.com/compilation/ios/">Compilation instructions</a>
+                    <a href="https://docs.libretro.com/development/retroarch/compilation/ios/">Compilation instructions</a>
                 </div>
             </div>
             <div class="col-md-3 col-sm-6">
@@ -139,8 +139,8 @@
                    <s><a href="https://buildbot.libretro.com/stable/<?php echo $version; ?>/apple/osx/x86/RetroArch.dmg" title="Use this if you are using a 32bit Mac with at least MacOS X 10.6 installed or later.">Download (Intel 32bit)</a></s>
                    <s><a href="https://buildbot.libretro.com/stable/<?php echo $version; ?>/apple/osx/x86/RetroArch_CG.dmg" title="Use this if you are using a 32bit Mac with at least MacOS X 10.6 installed or later. This build requires Nvidia Cg installed, and will use it. If in doubt, use the other one.">Download (Intel 32bit) (with Cg support - requires Cg installed)</a></s>
                    <a href="https://buildbot.libretro.com/stable/<?php echo $version; ?>/apple/osx/ppc/RetroArch.zip" title="Use this if you are using a PowerMac/Powerbook with MacOS X 10.5 installed.">Download (PowerPC)</a>
-                   <a href="https://docs.libretro.com/compilation/osx/">Compilation instructions (Intel)</a>
-                   <a href="https://docs.libretro.com/compilation/osxppc/">Compilation instructions (PowerPC)</a>
+                   <a href="https://docs.libretro.com/development/retroarch/compilation/osx/">Compilation instructions (Intel)</a>
+                   <a href="https://docs.libretro.com/development/retroarch/compilation/osxppc/">Compilation instructions (PowerPC)</a>
                    <p>Note: OSX version for x86 32bit will return at a later point in time. </p>
                 </div>
             </div>
@@ -152,7 +152,7 @@
                     <h4>PlayStation Vita / TV</h4>
                     <a href="https://buildbot.libretro.com/stable/<?php echo $version; ?>/playstation/vita/RetroArch.vpk">Download</a>
                     <a href="#">Installation instructions</a>
-                    <a href="https://docs.libretro.com/compilation/psvita/">Compilation instructions</a>
+                    <a href="https://docs.libretro.com/development/retroarch/compilation/psvita/">Compilation instructions</a>
                 </div>
             </div>
             <div class="col-md-3 col-sm-6">
@@ -161,7 +161,7 @@
                     <h4>PlayStation Portable</h4>
                     <a href="https://buildbot.libretro.com/stable/<?php echo $version; ?>/playstation/psp/RetroArch.7z">Download</a>
                     <a href="#">Installation instructions</a>
-                    <a href="https://docs.libretro.com/compilation/psp/">Compilation instructions</a>
+                    <a href="https://docs.libretro.com/development/retroarch/compilation/psp/">Compilation instructions</a>
                 </div>
             </div>
             <div class="col-md-3 col-sm-6">
@@ -169,7 +169,7 @@
                     <img src="images/ps2.png">
                     <h4>PlayStation 2</h4>
                     <a href="https://buildbot.libretro.com/stable/<?php echo $version; ?>/playstation/ps2/RetroArch_elf.7z">Download</a>
-                    <a href="https://docs.libretro.com/compilation/ps2/">Compilation instructions</a>
+                    <a href="https://docs.libretro.com/development/retroarch/compilation/ps2/">Compilation instructions</a>
                 </div>
             </div>
             <div class="col-md-3 col-sm-6">
@@ -195,7 +195,7 @@
                     <img src="images/nsw.png">
                     <h4>Switch</h4>
                     <a href="https://buildbot.libretro.com/stable/<?php echo $version; ?>/nintendo/switch/libnx/RetroArch.7z" title="Do NOT use SD cards formatted with exFAT, and always exit from 'Quit retroArch'">Download</a>
-                    <a href="https://docs.libretro.com/compilation/switch-libnx/">Compilation instructions</a>
+                    <a href="https://docs.libretro.com/development/retroarch/compilation/switch-libnx/">Compilation instructions</a>
                 </div>
             </div>
             <div class="col-md-3 col-sm-6">
@@ -205,7 +205,7 @@
                     <a href="https://buildbot.libretro.com/stable/<?php echo $version; ?>/nintendo/wiiu/RetroArch.7z">Download</a>
                     <a href="https://buildbot.libretro.com/stable/<?php echo $version; ?>/nintendo/wiiu/RetroArch_rpx.7z">Download (rpx)</a>
                     <a href="#">Installation instructions</a>
-                    <a href="https://docs.libretro.com/compilation/wiiu/">Compilation instructions</a>
+                    <a href="https://docs.libretro.com/development/retroarch/compilation/wiiu/">Compilation instructions</a>
                 </div>
             </div>
             <div class="col-md-3 col-sm-6">
@@ -214,7 +214,7 @@
                     <h4>Wii</h4>
                     <a href="https://buildbot.libretro.com/stable/<?php echo $version; ?>/nintendo/wii/RetroArch.7z">Download</a>
                     <a href="#">Installation instructions</a>
-                    <a href="https://docs.libretro.com/compilation/wii/">Compilation instructions</a>
+                    <a href="https://docs.libretro.com/development/retroarch/compilation/wii/">Compilation instructions</a>
                 </div>
             </div>
         </div>
@@ -225,7 +225,7 @@
                     <h4>GameCube</h4>
                     <a href="https://buildbot.libretro.com/stable/<?php echo $version; ?>/nintendo/ngc/RetroArch.7z">Download</a>
                     <a href="#">Installation instructions</a>
-                    <a href="https://docs.libretro.com/compilation/gamecube/">Compilation instructions</a>
+                    <a href="https://docs.libretro.com/development/retroarch/compilation/gamecube/">Compilation instructions</a>
                 </div>
             </div>
             <div class="col-md-3 col-sm-6">
@@ -235,7 +235,7 @@
                     <a href="https://buildbot.libretro.com/stable/<?php echo $version; ?>/nintendo/3ds/RetroArch_3dsx.7z">Download (3dsx)</a>
                     <a href="https://buildbot.libretro.com/stable/<?php echo $version; ?>/nintendo/3ds/RetroArch_cia.7z">Download (cia)</a>
                     <a href="#">Installation instructions</a>
-                    <a href="https://docs.libretro.com/compilation/3ds/">Compilation instructions</a>
+                    <a href="https://docs.libretro.com/development/retroarch/compilation/3ds/">Compilation instructions</a>
                 </div>
             </div>
             <div class="col-md-3 col-sm-6">
