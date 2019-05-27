c1a533d3cd195b16b75d88e49933c0d3f9855731
Twinaphex <libretro@gmail.com>
2019-05-27 16:26:58 +0200

    Update index.php

--- index.php
+++ index.php
@@ -10,6 +10,13 @@
         <link rel="stylesheet" href="font-awesome-4.7.0/css/font-awesome.min.css">
         <script src="jquery-3.2.0.min.js"></script>
         <script src="bootstrap/js/bootstrap.min.js"></script>
+        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({
+          google_ad_client: "ca-pub-9447404270680650",
+          enable_page_level_ads: true
+     });
+</script>
 
         <link href="https://fonts.googleapis.com/css?family=Hind" rel="stylesheet">
 
